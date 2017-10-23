f12e4f6930d1de8a261ecf29dab77671a03d4d76
digfish <pescadordigital@gmail.com>
2017-10-23 23:02:25 +0100

    Update ivx_config.php

--- config/ivx_config.php
+++ config/ivx_config.php
@@ -1,5 +1,5 @@
 <?php
 // MODIFY THIS FILE WITH YOUR SPECIFIC CONFIGURATION !!!
 
-define('IVX_DOMAIN','digfishprogramaes');
-define('IVX_TOKEN','a12cbdbfba0e6f90b076216c3a6b05cbcbda3cbc');
+define('IVX_DOMAIN','something');
+define('IVX_TOKEN','some*random*string');
